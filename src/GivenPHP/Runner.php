6c82a9436ae8c2f340394a3269aad46ab2a6d1ad
Thomas Ruiz <contact@thomasruiz.eu>
2014-06-16 14:47:44 +0200

    adding code coverage analysis

--- src/GivenPHP/Runner.php
+++ src/GivenPHP/Runner.php
@@ -2,6 +2,12 @@
 namespace GivenPHP;
 
 use Commando\Command;
+use Exception;
+use GivenPHP;
+use PHP_CodeCoverage;
+use PHP_CodeCoverage_Exception;
+use PHP_CodeCoverage_Filter;
+use PHP_CodeCoverage_Report_HTML;
 
 /**
  * Class Runner
@@ -12,23 +18,54 @@ class Runner
 {
 
     /**
+     * The PHP_CodeCoverage instance
+     *
+     * @var PHP_CodeCoverage $coverage
+     */
+    private $coverage = null;
+
+    /**
+     * True if the code coverage is activated, false otherwise
+     *
+     * @var boolean $has_coverage
+     */
+    private $has_coverage = null;
+
+    /**
      * Constructor
      */
     public function __construct()
     {
-        $this->arguments = new Command();
+        $this->cli = new Command();
+        $this->initialize_options();
+        $this->initialize_coverage_analysis();
     }
 
     /**
      * Run a test file
      *
+     * @throws PHP_CodeCoverage_Exception
      * @return void
      */
     public function run()
     {
-        $files = $this->arguments->getArgumentValues();
+        $files = $this->cli->getArgumentValues();
+
         foreach ($files AS $file) {
+            if ($this->has_coverage()) {
+                $this->coverage->start($file);
+            }
+
             $this->recursive_run($file);
+
+            if ($this->has_coverage()) {
+                $this->coverage->stop();
+            }
+        }
+
+        if ($this->has_coverage()) {
+            $writer = new PHP_CodeCoverage_Report_HTML;
+            $writer->process($this->coverage, '/tmp/code-coverage-report');
         }
     }
 
@@ -68,5 +105,52 @@ class Runner
         }
 
         closedir($dir);
+    }
+
+    /**
+     * Initialize the possible options of the CLI
+     *
+     * @return void
+     */
+    private function initialize_options()
+    {
+        $this->cli->option('coverage-html')
+            ->describedAs('Generate a code coverage report in HTML.');
+        $this->cli->option('coverage-clover')
+            ->describedAs('Generate a code coverage report in Clover XML.');
+    }
+
+    /**
+     * Return true if the code coverage is activated, false otherwise
+     *
+     * @throws Exception
+     * @return boolean
+     */
+    private function has_coverage()
+    {
+        if (!is_bool($this->has_coverage)) {
+            $this->has_coverage = $this->cli->getOption('coverage-html')->getValue() ||
+                                  $this->cli->getOption('coverage-clover')->getValue();
+        }
+
+        return $this->has_coverage;
+    }
+
+    /**
+     * Initialize the code coverage analysis
+     *
+     * @return void
+     */
+    private function initialize_coverage_analysis()
+    {
+        if (!$this->has_coverage()) {
+            return;
+        }
+
+        $filter = new PHP_CodeCoverage_Filter();
+        $filter->addDirectoryToBlacklist('vendor');
+        $filter->addFileToBlacklist(__DIR__ . '/../utils.php');
+
+        $this->coverage = new PHP_CodeCoverage(null, $filter);
     }
 }
