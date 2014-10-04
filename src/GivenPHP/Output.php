7172698bfc0cf4a05071191c3e8b7b226ab1cbe6
Richard Walker <digitalsadhu@gmail.com>
2014-10-04 08:57:32 +0200

    Add echo wrapper class with optional color

---
+++ src/GivenPHP/Output.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace GivenPHP;
+
+class Output {
+
+    const RED   = '[31m';
+    const BLUE  = '[34m';
+    const GREEN = '[32m';
+    const WHITE = '[0m';
+
+    public static function message($message, $color = null) {
+        if ($color !== null) echo chr(27) . $color;
+        echo $message;
+        echo chr(27) . Output::WHITE;
+    }
+}
